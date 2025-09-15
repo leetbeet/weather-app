b674df7433fc06cc07d89f0cfbf0124edd123900
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-09-15 12:38:53 +0800

    Optimise code

--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@ import './style.css';
 
 const getWeatherData = async (location) => {
   const loadingMsg = document.querySelector('.loading-msg');
+  const container = document.querySelector('.container');
   try {
-    const container = document.querySelector('.container');
-    container.className = 'container hidden';
-    loadingMsg.className = 'loading-msg';
+    container.classList.add('hidden');
+    loadingMsg.classList.remove('hidden');
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=REWV8CUHUMFRVEXFGDJADGCEL`
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(location.trim())}?key=REWV8CUHUMFRVEXFGDJADGCEL`
     );
 
     if (!response.ok) throw new Error(`Server error: ${response.status}`);
 
-    processWeatherData(response);
+    await processWeatherData(response);
   } catch (error) {
-    loadingMsg.className = 'loading-msg hidden';
+    loadingMsg.classList.add('hidden');
     alert(`Error fetching data, ${error.message}`);
   }
 };
@@ -34,22 +34,22 @@ const processWeatherData = async (response) => {
 
 const displayWeatherData = (weather) => {
   const loadingMsg = document.querySelector('.loading-msg');
-  loadingMsg.className = 'loading-msg hidden';
+  loadingMsg.classList.add('hidden');
   const container = document.querySelector('.container');
-  container.className = 'container';
+  container.classList.remove('hidden');
   const cloudy = document.querySelector('#cloudy');
   const city = document.querySelector('#city');
   const temp = document.querySelector('#temp');
   const feelsLike = document.querySelector('#feels-like');
   const humidity = document.querySelector('#humidity');
-  const percip = document.querySelector('#percip');
+  const precip = document.querySelector('#precip');
 
   cloudy.textContent = weather.condition;
   city.textContent = weather.name;
   temp.textContent = `${weather.temp} °C`;
   feelsLike.textContent = `Feels like: ${weather.feelsLike} °C`;
   humidity.textContent = `Humidity: ${weather.humidity}%`;
-  percip.textContent = `Precipitation chance: ${weather.precipChance}%`;
+  precip.textContent = `Precipitation chance: ${weather.precipChance}%`;
 };
 
 document.querySelector('form').addEventListener('submit', (e) => {
