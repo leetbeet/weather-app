189d19cc1a93057979a49bdedf46a89240492032
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-09-15 12:09:12 +0800

    Add function to display weather data

--- src/index.js
+++ src/index.js
@@ -10,21 +10,39 @@ const getWeatherData = async (location) => {
 
     processWeatherData(response);
   } catch (error) {
-    console.error('Error fetching data:', error.message);
+    alert(`Error fetching data, ${error.message}`);
   }
 };
 
 const processWeatherData = async (response) => {
   const data = await response.json();
-  return {
-    temp: data.currentConditions.temp,
+  const weather = {
+    name: data.resolvedAddress,
+    temp: (((data.currentConditions.temp - 32) * 5) / 9).toFixed(1),
     condition: data.currentConditions.conditions,
-    feelsLike: data.currentConditions.feelslike,
+    feelsLike: (((data.currentConditions.feelslike - 32) * 5) / 9).toFixed(1),
     humidity: data.currentConditions.humidity,
-    windSpeed: data.currentConditions.windspeed,
-    windDir: data.currentConditions.winddir,
     precipChance: data.currentConditions.precipprob,
   };
+  displayWeatherData(weather);
+};
+
+const displayWeatherData = (weather) => {
+  const container = document.querySelector('.container');
+  container.className = 'container';
+  const cloudy = document.querySelector('#cloudy');
+  const city = document.querySelector('#city');
+  const temp = document.querySelector('#temp');
+  const feelsLike = document.querySelector('#feels-like');
+  const humidity = document.querySelector('#humidity');
+  const percip = document.querySelector('#percip');
+
+  cloudy.textContent = weather.condition;
+  city.textContent = weather.name;
+  temp.textContent = `${weather.temp} °C`;
+  feelsLike.textContent = `Feels like: ${weather.feelsLike} °C`;
+  humidity.textContent = `Humidity: ${weather.humidity}%`;
+  percip.textContent = `Precipitation chance: ${weather.precipChance}%`;
 };
 
 document.querySelector('form').addEventListener('submit', (e) => {
