02ee510460c3cf3d157e1a8f9cf1031eef92b199
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-09-15 09:30:44 +0800

    Add function to fetch weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-import "./style.css";
+import './style.css';
+
+const getWeatherData = async (location) => {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=REWV8CUHUMFRVEXFGDJADGCEL`
+    );
+
+    if (!response.ok) throw new Error(`Server error: ${response.status}`);
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+  }
+};
